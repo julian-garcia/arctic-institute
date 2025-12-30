901e1d46395acbb57570b1fca5b9d56878bc731c
Julian Garcia <julian.garcia.leoni@gmail.com>
2025-12-30 00:51:15 +0000

    Fix publication types listener

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,32 +1,38 @@
-import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
-import '@fortawesome/fontawesome-free/scss/brands.scss';
-import '@fortawesome/fontawesome-free/scss/solid.scss';
-import 'cookieconsent/build/cookieconsent.min.js';
-import 'cookieconsent/build/cookieconsent.min.css';
-import './main.css';
-import './style/main.scss';
-import './script/values';
-import './script/approach';
-import './script/search';
-import './script/calendar';
-import './script/experts';
-import './script/cookie-consent';
-import './script/publication-filter';
-import './script/popups';
-import {hideSearch} from './script/search';
+import "@fortawesome/fontawesome-free/scss/fontawesome.scss";
+import "@fortawesome/fontawesome-free/scss/brands.scss";
+import "@fortawesome/fontawesome-free/scss/solid.scss";
+import "cookieconsent/build/cookieconsent.min.js";
+import "cookieconsent/build/cookieconsent.min.css";
+import "./main.css";
+import "./style/main.scss";
+import "./script/values";
+import "./script/approach";
+import "./script/search";
+import "./script/calendar";
+import "./script/experts";
+import "./script/cookie-consent";
+import "./script/publication-filter";
+import "./script/popups";
+import { hideSearch } from "./script/search";
 
-if ( window.history.replaceState ) {
-  window.history.replaceState( null, null, window.location.href );
+if (window.history.replaceState) {
+  window.history.replaceState(null, null, window.location.href);
 }
 
-window.addEventListener('resize', () => { hideSearch(); });
+window.addEventListener("resize", () => {
+  hideSearch();
+});
 
-const publicationTypes = document.querySelector('.publication-types');
+const publicationTypes = document.querySelector(".publication-types");
 if (publicationTypes) {
-  publicationTypes.addEventListener('click', (e) => {
-    document.querySelectorAll('.publication-types .type-text')
-      .forEach(type => type.style.display = "none");
-    const pubTypeElement = document.querySelector('.publication-types .' + e.target.textContent.replace(/\s/g, '-'));
+  publicationTypes.addEventListener("click", (e) => {
+    document
+      .querySelectorAll(".publication-types .type-text")
+      .forEach((type) => (type.style.display = "none"));
+    const pubTypeElement = document.querySelector(
+      ".publication-types ." +
+        e.target.textContent.replace(/\s/g, "-").replace(/[^a-zA-Z0-9-]/g, "")
+    );
     pubTypeElement.style.display = "block";
   });
 }
